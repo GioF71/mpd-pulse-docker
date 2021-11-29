9d4cb3373b5a5e6d2bcaa8446e2498b58a5b8843
GioF_71 <giovanni.fulco@gmail.com>
2021-11-30 00:51:40 +0100

    removed unnecessary scripts

--- run-interactive.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -t -i \
-  --rm \
-  -u $(id -u):$(id -g) \
-  --name mpd-pulse \
-  --network=host \
-  --volume=/run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
-  giof71/mpd-pulse:latest
