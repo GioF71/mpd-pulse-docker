4ff5fe681730d2b0d981c6ad6a13812302bc8495
GioF_71 <giovanni.fulco@gmail.com>
2021-11-30 00:18:14 +0100

    Added qobuz and tidal variables

--- assets/run-mpd.sh
+++ assets/run-mpd.sh
@@ -61,18 +61,18 @@ sed -i 's/USER_NAME/'"$USER_NAME"'/g' /etc/mpd.conf
 
 cat /etc/mpd.conf
 
-#sed -i 's/QOBUZ_PLUGIN_ENABLED/'"$QOBUZ_PLUGIN_ENABLED"'/g' /etc/mpd.conf
-#sed -i 's/QOBUZ_APP_ID/'"$QOBUZ_APP_ID"'/g' /etc/mpd.conf
-#sed -i 's/QOBUZ_APP_SECRET/'"$QOBUZ_APP_SECRET"'/g' /etc/mpd.conf
-#sed -i 's/QOBUZ_USERNAME/'"$QOBUZ_USERNAME"'/g' /etc/mpd.conf
-#sed -i 's/QOBUZ_PASSWORD/'"$QOBUZ_PASSWORD"'/g' /etc/mpd.conf
-#sed -i 's/QOBUZ_FORMAT_ID/'"$QOBUZ_FORMAT_ID"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_PLUGIN_ENABLED/'"$QOBUZ_PLUGIN_ENABLED"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_APP_ID/'"$QOBUZ_APP_ID"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_APP_SECRET/'"$QOBUZ_APP_SECRET"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_USERNAME/'"$QOBUZ_USERNAME"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_PASSWORD/'"$QOBUZ_PASSWORD"'/g' /etc/mpd.conf
+sed -i 's/QOBUZ_FORMAT_ID/'"$QOBUZ_FORMAT_ID"'/g' /etc/mpd.conf
 
-#sed -i 's/TIDAL_PLUGIN_ENABLED/'"$TIDAL_PLUGIN_ENABLED"'/g' /etc/mpd.conf
-#sed -i 's/TIDAL_APP_TOKEN/'"$TIDAL_APP_TOKEN"'/g' /etc/mpd.conf
-#sed -i 's/TIDAL_USERNAME/'"$TIDAL_USERNAME"'/g' /etc/mpd.conf
-#sed -i 's/TIDAL_PASSWORD/'"$TIDAL_PASSWORD"'/g' /etc/mpd.conf
-#sed -i 's/TIDAL_AUDIOQUALITY/'"$TIDAL_AUDIOQUALITY"'/g' /etc/mpd.conf
+sed -i 's/TIDAL_PLUGIN_ENABLED/'"$TIDAL_PLUGIN_ENABLED"'/g' /etc/mpd.conf
+sed -i 's/TIDAL_APP_TOKEN/'"$TIDAL_APP_TOKEN"'/g' /etc/mpd.conf
+sed -i 's/TIDAL_USERNAME/'"$TIDAL_USERNAME"'/g' /etc/mpd.conf
+sed -i 's/TIDAL_PASSWORD/'"$TIDAL_PASSWORD"'/g' /etc/mpd.conf
+sed -i 's/TIDAL_AUDIOQUALITY/'"$TIDAL_AUDIOQUALITY"'/g' /etc/mpd.conf
 
 #sed -i 's/REPLAYGAIN_MODE/'"$REPLAYGAIN_MODE"'/g' /etc/mpd.conf
 #sed -i 's/REPLAYGAIN_PREAMP/'"$REPLAYGAIN_PREAMP"'/g' /etc/mpd.conf
@@ -80,7 +80,7 @@ cat /etc/mpd.conf
 #sed -i 's/REPLAYGAIN_LIMIT/'"$REPLAYGAIN_LIMIT"'/g' /etc/mpd.conf
 #sed -i 's/VOLUME_NORMALIZATION/'"$VOLUME_NORMALIZATION"'/g' /etc/mpd.conf
 
-#cat /etc/mpd.conf
+cat /etc/mpd.conf
 
 echo "About to sleep for $STARTUP_DELAY_SEC second(s)"
 sleep $STARTUP_DELAY_SEC
